931ba7f214a3090a69388719cc8a0fc17f91576b
Mathias Alvez <Mathias@MacBook-Pro.local>
2019-03-06 18:46:22 +0100

    Final Modification

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require("express");
+const express = require("express");
 var app = express();
 var bodyParser  = require("body-parser");
 
@@ -7,17 +7,42 @@ app.use(bodyParser.json());
 
 var router = express.Router();
 
+var request = require("request");
+
 router.get('/hora', function(req, res) {
 
-  var d = new Date();
+  var hora = new Date();
 
-    res.send("La hora es: " + d.getHours() + ":" + d.getMinutes() + ":" +d.getSeconds());
+    res.send("La hora es: " + hora.getHours() + ":" + hora.getMinutes() + ":" +hora.getSeconds());
    
 });
 
-router.get('/estrellas', function(req, res) {
-
+router.get('/stars',  function(req, res) {
+  httpGet({url: "https://api.github.com/repos/Malebo29/Malebo29.github.io", headers: {"User-Agent": "request"}}).then (result => {
+  
+    res.send(`Las estrellas de mi repo son: ${result}`);
+  
+  })
+  
 });
+
+function httpGet(theUrl)
+{
+  
+return new Promise((res, reject) => {
+  
+  request (theUrl, function(error, response, body){
+    if (!error && response.statusCode == 200){
+      res (JSON.parse(body).stargazers_count);
+    }
+    else {
+      console.log (response.statusCode);
+      console.log (error);
+      res ({message: "algo fue mal"});
+    }
+  })
+})
+}
 
 app.use(router);
 
